eea677e47f5e2978609b35db5692a136318e8af5
markman-1111 <captain_tripps@hotmail.com>
2017-06-28 14:45:05 -0400

    Add files via upload

--- Random.rb
+++ Random.rb
@@ -1,1 +1,19 @@
-10.times.map{ Random.rand(9) }
+def call
+1.times.map{ Random.rand(9) }
+end
+ 
+def guess
+	number = gets.chomp
+end
+
+def result
+	if number == call
+		then p "Winner!"
+	else p "Loser!"
+	end
+end
+
+p "Take a guess"
+guess
+puts call
+result
